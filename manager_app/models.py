295599e2f4868b957b92f96d9d9974c36868a2b9
Maksym Buslenko <maksbusl@gmail.com>
2025-08-26 21:45:10 +0300

    create a Redactor model

--- manager_app/models.py
+++ manager_app/models.py
@@ -1,6 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
 
 class Topic(models.Model):
     name = models.CharField(
@@ -14,4 +14,19 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(
+        default=0,
+        blank=True,
+        verbose_name="Years of Experience",
+    )
+
+    class Meta:
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+
+    def __str__(self):
+        return f"{self.username} {self.first_name} {self.last_name}"
 
