b6d2535a27d06c57da05c7658ca697af1f0260e4
Maksym Buslenko <maksbusl@gmail.com>
2025-08-26 21:44:17 +0300

    create a Topic model

--- manager_app/models.py
+++ manager_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(
+        max_length=255,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = "topic"
+        verbose_name_plural = "topics"
+
+    def __str__(self):
+        return self.name
+
