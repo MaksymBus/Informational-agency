127d84c322bb72a91c0bf6630f8fde7cbf194922
Maksym Buslenko <maksbusl@gmail.com>
2025-08-27 16:40:49 +0300

    add base url path manager_app and accounts in urlpatterns

--- informational_agency/urls.py
+++ informational_agency/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path("admin/", admin.site.urls),
+    path("", include("manager_app.urls", namespace="manager_app")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
