ae53e1973a043e10a6b55c8fe379f31a664d63b8
Maksym Buslenko <maksbusl@gmail.com>
2025-08-28 21:36:18 +0300

    update settings.py add debug in templates

--- informational_agency/settings.py
+++ informational_agency/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
@@ -73,11 +74,9 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "informational_agency.wsgi.application"
-
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
-
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+WSGI_APPLICATION = "informational_agency.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
